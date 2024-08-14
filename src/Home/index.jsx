87ddc4addea2707d16dc05ff27c88403fa0dd4ea
MatheusKiyota <matheus.kiyota@estudante.ifms.edu.br>
2024-08-14 11:35:53 -0300

    modificação 14/08

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -2,15 +2,18 @@ import Header from "../components/Header";
 import {Carousel} from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import "./../globals.css";
-
+import Footer from "../components/Footer";
 
 
 
 export default function Home() {
     return(
         <>
-                <h1 className="Home">Stussy</h1>
+            <div className="Home">
+                <img className="fotoStussy" src="src/fotos/bola8.png"/> 
+           </div>
                   <Header/>
+
                     <Carousel 
                         infiniteLoop
                         useKeyboardArrows
@@ -22,18 +25,13 @@ export default function Home() {
                         dynamicHeight
                     >
                         <div>
-                            <img src="hhttps://cactusteals.com.br/cdn/shop/collections/5_1850x.png?v=1690590895ttps://acdn.mitiendanube.com/stores/001/167/965/themes/new_linkedman/2-slide-1703944594918-4516444529-9f64ef0073396b62ffc4a2c72c6931721703944595-1920-1920.webp?257411524"/>
+                            <img src="src/fotos/foto.stussy.jpg"/>
                         </div>
 
-                        <div>
-                            <img src="https://acdn.mitiendanube.com/stores/001/167/965/themes/new_linkedman/2-slide-1722260379840-8630136989-b285f3135c3be28d3f552e3eabf60dbb1722260380-1920-1920.webp?257411524"/>
-                        </div>
-
-                        <div>
-                            <img src="https://acdn.mitiendanube.com/stores/001/167/965/themes/new_linkedman/2-slide-1717083598973-565208895-b3d411850afdbfeff7870647ef0c36771717083599-1920-1920.webp?257411524://acdn.mitiendanube.com/stores/001/167/965/themes/new_linkedman/2-slide-1703944594918-4516444529-9f64ef0073396b62ffc4a2c72c6931721703944595-1920-1920.webp?257411524"/>
-                        </div>
+                    
                     </Carousel>
-                 
+
+                    <Footer/>
         </>
                     
     );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+import "../globals.css";
+
+export default function Footer() {
+    return(
+        <div className="rodape"> 
+            <img className="stussyBranco" src="src/fotos/stussylogo.png"/>
+
+            
+        </div>
+    ); 
+}
