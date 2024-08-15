55ca5a8e0459d29096e5693fb2ecf6da64ea1f95
MatheusKiyota <matheus.kiyota@estudante.ifms.edu.br>
2024-08-14 23:16:58 -0300

    modificaçao 14/08

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,38 +1,59 @@
+import React, { useState } from "react";
 import Header from "../components/Header";
-import {Carousel} from 'react-responsive-carousel';
+import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import "./../globals.css";
 import Footer from "../components/Footer";
-
-
+import ListarProdutos from "../components/ListarProdutos";
 
 export default function Home() {
-    return(
+
+    const [ValoresCamisas] = useState([
+        {
+            id: 1,
+            titulo: "camisa 1",
+            valor: "R$50,00"
+        },
+        {
+            id: 2,
+            titulo: "camisa 2",
+            valor: "R$50,00"
+        },
+        {
+            id: 3,
+            titulo: "camisa 3",
+            valor: "R$50,00"
+        }
+    ]);
+
+    return (
         <>
             <div className="Home">
-                <img className="fotoStussy" src="src/fotos/bola8.png"/> 
-           </div>
-                  <Header/>
+                <img className="fotoStussy" src="src/fotos/bola8.png" alt="Imagem de bola 8" />
+            </div>
+            <Header />
 
-                    <Carousel 
-                        infiniteLoop
-                        useKeyboardArrows
-                        autoPlay
-                        showArrows={true}
-                        showStatus={false}
-                        showThumbs={false}
-                    
-                        dynamicHeight
-                    >
-                        <div>
-                            <img src="src/fotos/foto.stussy.jpg"/>
-                        </div>
+            <Carousel
+                infiniteLoop
+                useKeyboardArrows
+                autoPlay
+                showArrows={true}
+                showStatus={false}
+                showThumbs={false}
+                dynamicHeight
+            >
+                <div>
+                    <img src="src/fotos/foto.stussy.jpg" alt="Imagem Stussy" />
+                </div>
+            </Carousel>
 
-                    
-                    </Carousel>
+            <div>
+                <ListarProdutos produtos={ValoresCamisas} />
+            </div>
 
-                    <Footer/>
+            <Footer />
         </>
-                    
     );
 }
+
+
--- src/components/ListarProdutos.jsx
+++ src/components/ListarProdutos.jsx
@@ -1,3 +1,18 @@
+import React from "react";
 import { Link } from "react-router-dom";
 import "../globals.css";
 
+export default function ListarProdutos({ produtos }) {
+    return (
+        <div>
+            {
+                produtos.map((item) =>
+                    <div className="container"key={item.id}>
+                        <p>{item.titulo}</p>
+                        <p>{item.valor}</p>
+                    </div>
+                )
+            }
+        </div>
+    );
+}
